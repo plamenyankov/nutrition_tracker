8e75da50aff58ff459410b48c31bb571b3cc48a1
Plamen Yankov <p.s.yankov@abv.bg>
2023-10-30 18:25:58 +0200

    crud db

--- app.py
+++ app.py
@@ -1,177 +1,46 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for, flash
 import pandas as pd
-from datetime import datetime
-import io
-import sqlite3
-from models import openai_utils
-# from models.food import save_to_database, fetch_all_nutritions
 from models.food import FoodDatabase
-
-
-
+from models.foods.food_blueprint import food_blueprint
 
 
 app = Flask(__name__)
 app.secret_key = 'secret'  # This is for flash messaging
-temp_df = None
+app.register_blueprint(food_blueprint)
 food_db = FoodDatabase()
-@app.route('/ingr')
-def get_unit():
-    unit_id = food_db.save_ingredient('chicken')
-    return jsonify(unit_id)
 
-@app.route('/nutrition', methods=['GET'])
-def get_nutrition():
-    with sqlite3.connect('database.db') as conn:
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM nutrition')
-        results = cursor.fetchall()
-    return jsonify(results)
+
 @app.route('/', methods=["GET", "POST"])
 def home():
 
-    df = pd.read_csv('data/data.csv')
-    df2 = pd.read_csv('data/iwatch.csv')
-    data = df.to_dict(orient='records')
-    columns = df.columns
+    consumption = food_db.fetch_all_consumption()
+    if len(consumption) > 0:
+        df = pd.DataFrame(consumption)
+        df[['protein','carb']] = df[['protein','carb']] * 4
+        df['fat'] = df['fat'] * 9
 
-    # Create a bar chart
-    # Group by date and sum up the necessary columns
-    grouped_data = df.groupby('date').agg({
-        'protein': 'sum',
-        'fats': 'sum',
-        'carbs': 'sum'
-    }).reset_index()
-    kcals = df2.groupby('date')['kcal'].sum().tolist()
-    dates = grouped_data['date'].tolist()
-    proteins = grouped_data['protein'].tolist()
-    fats = grouped_data['fats'].tolist()
-    carbs = grouped_data['carbs'].tolist()
+        data = df.to_dict(orient='records')
+        columns = df.columns
 
-    return render_template('input_form.html', data=data, columns=columns, dates=dates, kcals=kcals, proteins=proteins, fats=fats, carbs=carbs)
+        # Create a bar chart
+        # Group by date and sum up the necessary columns
+        grouped_data = df.groupby('date').agg({
+            'protein': 'sum',
+            'fat': 'sum',
+            'carb': 'sum',
+            'kcal':'sum'
+        }).reset_index()
 
-@app.route('/food', methods=['GET','POST'])
-def food():
-    global temp_df
-    data = None
-    columns = None
-    if temp_df is not None:
-        current_date = datetime.now().strftime('%d.%m.%Y')
-        temp_df['date'] = current_date
-        data = temp_df.to_dict(orient='records')
-        columns = temp_df.columns
-    all_nutritions = food_db.fetch_all_nutrition()
-    all_consumption = food_db.fetch_all_consumption()
-    return render_template('food.html', data=data, columns=columns,  nutritions= all_nutritions, consumption=all_consumption)
-@app.route('/preview', methods=["GET", "POST"])
-def preview():
-    global temp_df
-    data = None
-    columns = None
-    if temp_df is not None:
-        current_date = datetime.now().strftime('%d.%m.%Y')
-        temp_df['date'] = current_date
-        data = temp_df.to_dict(orient='records')
-        columns = temp_df.columns
-    return render_template('preview.html', data=data, columns=columns)
-@app.route('/submit', methods=['POST'])
-def submit():
-    global temp_df
-
-    user_input = request.form['foods']
-
-    # Call OpenAI API here
-    response = openai_utils.get_openai_response(user_input)
-
-    print('Response', response)
-    # Convert the response to a DataFrame
-    data_io = io.StringIO(response)
-    temp_df = pd.read_csv(data_io)
-
-    return redirect(url_for('food'))
-
-@app.route('/remove_ingredient', methods=['POST'])
-def remove_ingredient():
-    # This will return a list of all the ingredient_ids from the checked checkboxes
-    ingredient_ids_to_remove = request.form.getlist('remove_ids[]')
-
-    # Now you can iterate over this list and process each ID as needed
-    for ingredient_id in ingredient_ids_to_remove:
-        # Your logic to delete or process the ingredient_id goes here
-        food_db.delete_ingredient(ingredient_id)
-
-    return redirect(url_for('food'))
-
-@app.route('/remove_consumption', methods=['POST'])
-def remove_consumption():
-    # This will return a list of all the ingredient_ids from the checked checkboxes
-    ingredient_ids_to_remove = request.form.getlist('remove_ids[]')
-
-    # Now you can iterate over this list and process each ID as needed
-    for ingredient_id in ingredient_ids_to_remove:
-        # Your logic to delete or process the ingredient_id goes here
-        food_db.delete_consumption(ingredient_id)
-
-    return redirect(url_for('food'))
-
-
-def save_ingredients(date, form):
-    global temp_df
-
-    temp_df.columns = temp_df.columns.str.strip()
-
-    ingredient_quantity_ids = food_db.save_to_database(temp_df.to_csv(index=False))
-    return ingredient_quantity_ids
-def save_recipe(date, form):
-    global temp_df
-    serv = 1
-
-    if form['recipe'] is None:
-        return redirect(url_for('food'))
-    if form['serv'] is not None:
-        serv = form['serv']
-
-    recipe = form['recipe']
-
-    temp_df.columns = temp_df.columns.str.strip()
-
-    ingredient_quantity_ids = food_db.save_recipe(date, recipe, serv, temp_df.to_csv(index=False))
-    return ingredient_quantity_ids
-@app.route('/save_food', methods=['POST'])
-def save_food():
-    global temp_df
-    button_clicked = request.form['action']
-    ingredient_quantity_ids = []
-    date = datetime.now().strftime('%d.%m.%Y')
-    if request.form['date'] is None:
-        date = request.form['date']
-
-    if button_clicked == "Save Ingredients":
-        ingredient_quantity_ids = save_ingredients(date, request.form)
-    elif button_clicked == "Save as Recipe":
-        ingredient_quantity_ids = save_recipe(date, request.form)
-    else:
-        # Logic for cancel, e.g., redirect to another page
-        temp_df = None
-        return redirect(url_for('food'))
-
-    if request.form['consumption'] is not None:
-        for ingredient_quantity_id in ingredient_quantity_ids:
-            food_db.save_consumption(date, ingredient_quantity_id)
-    temp_df = None
-    return redirect(url_for('food'))
+        kcals = grouped_data['kcal'].tolist()
+        dates = grouped_data['date'].tolist()
+        proteins = grouped_data['protein'].tolist()
+        fats = grouped_data['fat'].tolist()
+        carbs = grouped_data['carb'].tolist()
 
 
 
-@app.route('/chart')
-def chart():
-    # Load data from CSV
-    df = pd.read_csv('data/data.csv')
+    return render_template('index.html', data=data, columns=columns, dates=dates, kcals=kcals, proteins=proteins, fats=fats, carbs=carbs)
 
-    # Data processing for chart (group by date, summarize, etc.) goes here
-
-    # Render chart - you can use a library like Plotly or Matplotlib to generate and show charts
-    return "Chart goes here!"
 
 
 if __name__ == "__main__":
--- models/openai_utils.py
+++ models/openai_utils.py
@@ -1,4 +1,15 @@
 import openai
+def cleanup_csv(api_response):
+    lines = api_response.strip().split("\n")
+    cleaned_lines = []
+
+    expected_columns = 9  # Based on the example you provided.
+
+    for line in lines:
+        if len(line.split(",")) == expected_columns:
+            cleaned_lines.append(line)
+
+    return "\n".join(cleaned_lines)
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     openai.api_key = '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'
@@ -14,8 +25,19 @@ def get_openai_response(user_input):
         calculate calories and macronutrients of the following ingredients into the triple backquotes:
         ```{user_input}```
         Write in the csv format: quantity,measurement,ingredient,carbs,fats,proteins,net_carbs,fiber,kcal.
-        Examples: 1 large banana
+        Examples 1: 1 large banana, 30g cream cheese, 100g oat flakes, 20g shredded coconut
         qty,unit,ingr,carbs,fats,protein,net_carbs,fiber,kcal
         1,large,banana,27,0.4,1.3,23.9,3.1,121
+        30,g,cream cheese,0.7,12.3,1.6,0.7,0,120.4
+        100,g,oat flakes,55.1,6.9,14.2,1.2,12.3,364
+        20,g,shredded coconut,1.5,12.2,1.2,1.3,0,130
+        Example 2: 150g sour cream 20% fat
+        qty,unit,ingr,carbs,fats,protein,net_carbs,fiber,kcal
+        150,g,sour cream 20% fat,2.1,30,3.6,2.1,0,315
         """
-    return get_completion(prompt)
+    response = get_completion(prompt)
+    print("Raw response:", response)
+    cleaned_response = cleanup_csv(response)
+    return cleaned_response
+
+# 3 large egg, 1 large banana, 30g cream cheese, 20g shredded coconuts, 10g flax seeds, 10g chia seeds, 150g oat flakes, 10g cocoa powder, 100g erythritol
